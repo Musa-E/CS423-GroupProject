e96b03ffcc4587ccc0a43f8ee8950772baf1a209
pollythepocket <pollyruhnke@gmail.com>
2024-09-28 10:34:57 -0500

    added unpopulated listview

--- Demo/src/main/java/com/myscript/iink/demo/TaskListView.java
+++ Demo/src/main/java/com/myscript/iink/demo/TaskListView.java
@@ -1,4 +1,15 @@
 package com.myscript.iink.demo;
 
-public class TaskListView {
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TaskListView extends Activity {
+
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu_task_list);
+
+
+    }
 }
