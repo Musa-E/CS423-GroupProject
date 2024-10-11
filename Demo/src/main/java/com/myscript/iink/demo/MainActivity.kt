0e51b67aa9ce235b154e1160cc73948ffd213956
pollythepocket <pollyruhnke@gmail.com>
2024-10-11 13:14:49 -0500

    changed active pen to inverse

--- Demo/src/main/java/com/myscript/iink/demo/MainActivity.kt
+++ Demo/src/main/java/com/myscript/iink/demo/MainActivity.kt
@@ -233,9 +233,10 @@ class MainActivity : AppCompatActivity() {
 
         //sets up the entire viewmodel, do not touch
         viewModel.enableActivePen.observe(this) { activePenEnabled ->
-            binding.editorToolbar.switchActivePen.isChecked = activePenEnabled
+            binding.editorToolbar.switchActivePen.isChecked = !activePenEnabled
             isPenActivated = activePenEnabled;
         }
+
         viewModel.error.observe(this, this::onError)
         viewModel.toolSheetExpansionState.observe(this, this::onToolSheetExpansionStateUpdate)
         viewModel.availableTools.observe(this, this::onAvailableToolsUpdate)
@@ -572,7 +573,7 @@ class MainActivity : AppCompatActivity() {
 
         with(binding.editorToolbar) {
             switchActivePen.setOnCheckedChangeListener { _, isChecked ->
-                viewModel.enableActivePen(isChecked)
+                viewModel.enableActivePen(!isChecked)
                 isPenActivated = true;
             }
             editorUndo.setOnClickListener { viewModel.undo() }
