bf327297efe1652f1fd8aa5c282299db6fa6f442
Musa-E <melqa2@uic.edu>
2024-11-11 03:04:14 -0600

    Added the gesture help button
    This adds the handling of the gesture help screen.  The previous commit added the supporting resources.  Nothing else aside from this file, and the three files in the resource folder (strings.xml, main_activity.xml, and gesture_help_dialog.xml) were modified; I just re-added all the folders for convenience.

--- Demo/src/main/java/com/myscript/iink/demo/MainActivity.kt
+++ Demo/src/main/java/com/myscript/iink/demo/MainActivity.kt
@@ -19,7 +19,6 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.MotionEvent
 import android.view.View
-import android.view.WindowManager
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
@@ -958,6 +957,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
     /**
      * showGestureHelpDialog()
      * @return Displays a dialog box with information on the different recognizable gestures
@@ -1020,6 +1020,5 @@ class MainActivity : AppCompatActivity() {
             nextButton.visibility = Button.VISIBLE
         }
     }
-
 
 }
